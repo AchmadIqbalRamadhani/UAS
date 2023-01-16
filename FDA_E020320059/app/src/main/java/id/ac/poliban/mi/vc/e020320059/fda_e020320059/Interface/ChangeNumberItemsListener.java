2015792ab4bf135873db0fbc1ab1714fd73ef58f
AchmadIqbalRamadhani <iramadhani414@gmail.com>
2023-01-16 18:33:09 +0800

    first commit

---
+++ FDA_E020320059/app/src/main/java/id/ac/poliban/mi/vc/e020320059/fda_e020320059/Interface/ChangeNumberItemsListener.java
@@ -0,0 +1,5 @@
+package id.ac.poliban.mi.vc.e020320059.fda_e020320059.Interface;
+
+public interface ChangeNumberItemsListener {
+    void changed();
+}
---
+++ Project77_E020320059/app/src/main/java/id/ac/poliban/mi/vc/e020320059/project77_e020320059/MainActivity.java
@@ -0,0 +1,14 @@
+package id.ac.poliban.mi.vc.e020320059.project77_e020320059;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
